0c8109fc097e1831091bbb651e7b1440c3f7bae5
kate8mart <martyanova.katerina@gmail.com>
2020-03-10 13:59:23 -0400

    Create smooth_signal.py

---
+++ smooth_signal.py
@@ -0,0 +1,42 @@
+def smooth_signal(x,window_len=10,window='flat'):
+
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    The code taken from: https://scipy-cookbook.readthedocs.io/items/SignalSmooth.html
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+                'flat' window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal        
+    """
+
+    if x.ndim != 1:
+        raise(ValueError, "smooth only accepts 1 dimension arrays.")
+
+    if x.size < window_len:
+        raise(ValueError, "Input vector needs to be bigger than window size.")
+
+    if window_len<3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise(ValueError, "Window is one of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+
+    if window == 'flat': # Moving average
+        w=np.ones(window_len,'d')
+    else:
+        w=eval('np.'+window+'(window_len)')
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+
+    return y[(int(window_len/2)-1):-int(window_len/2)]
