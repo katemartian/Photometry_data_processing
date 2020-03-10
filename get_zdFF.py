ff06b5d2de6152d604e1bfaccf235a26ff351336
kate8mart <martyanova.katerina@gmail.com>
2020-03-10 13:58:14 -0400

    Update get_zdFF.py

--- get_zdFF.py
+++ get_zdFF.py
@@ -1,6 +1,6 @@
 '''
 get_zdFF.py calculates standardized dF/F signal based on calcium-idependent 
-and calcium-dependent signals comomnly recorded using fiber photometry calcium imaging
+and calcium-dependent signals commonly recorded using fiber photometry calcium imaging
 
 Ocober 2019 Ekaterina Martianova ekaterina.martianova.1@ulaval.ca 
 
@@ -12,52 +12,53 @@ Reference:
 
 '''
 
-def get_zdFF(raw_reference,raw_signal,smooth_win,remove,lambd,porder,itermax): 
+def get_zdFF(reference,signal,smooth_win=10,remove=200,lambd=5e4,porder=1,itermax=50): 
   '''
   Calculates z-score dF/F signal based on fiber photometry calcium-idependent 
   and calcium-dependent signals
   
   Input
-      raw_reference - calcium-independent signal (usually 405-420 nm excitation)
-      raw_signal - calcium-dependent signal (usually 465-490 nm excitation for 
-                   green fluorescent proteins, or ~560 nm for red)
-      smooth_win - window for moving average smooth 
-      remove - the beginning of the traces with a big slope one would like to remove
+      reference: calcium-independent signal (usually 405-420 nm excitation), 1D array
+      signal: calcium-dependent signal (usually 465-490 nm excitation for 
+                   green fluorescent proteins, or ~560 nm for red), 1D array
+      smooth_win: window for moving average smooth, integer
+      remove: the beginning of the traces with a big slope one would like to remove, integer
       Inputs for airPLS:
-      lambd - parameter that can be adjusted by user. The larger lambda is,  
+      lambd: parameter that can be adjusted by user. The larger lambda is,  
               the smoother the resulting background, z
-      porder - adaptive iteratively reweighted penalized least squares for baseline fitting
+      porder: adaptive iteratively reweighted penalized least squares for baseline fitting
       itermax: maximum iteration times
   Output
-      zdFF - z-score dF/F, numpy vector
+      zdFF - z-score dF/F, 1D numpy array
   '''
   
   import numpy as np
   from sklearn.linear_model import Lasso
 
  # Smooth signal
-  reference = np.array(raw_reference.rolling(window=smooth_win).mean()).reshape(len(raw_reference),1)
-  signal = np.array(raw_signal.rolling(window=smooth_win).mean()).reshape(len(raw_signal),1)
+  reference = smooth_signal(reference, smooth_win)
+  signal = smooth_signal(signal, smooth_win)
   
  # Remove slope using airPLS algorithm
-  r_base=airPLS(raw_reference.T,lambda_=lambd,porder=porder,itermax=itermax).reshape(len(raw_reference),1)
-  s_base=airPLS(raw_signal.T,lambda_=lambd,porder=porder,itermax=itermax).reshape(len(raw_signal),1)  
+  r_base=airPLS(reference,lambda_=lambd,porder=porder,itermax=itermax)
+  s_base=airPLS(signal,lambda_=lambd,porder=porder,itermax=itermax) 
 
  # Remove baseline and the begining of recording
   reference = (reference[remove:] - r_base[remove:])
   signal = (signal[remove:] - s_base[remove:])   
 
  # Standardize signals    
-  z_reference = (reference - np.median(reference)) / np.std(reference)
-  z_signal = (signal - np.median(signal)) / np.std(signal)
+  reference = (reference - np.median(reference)) / np.std(reference)
+  signal = (signal - np.median(signal)) / np.std(signal)
   
  # Align reference signal to calcium signal using non-negative robust linear regression
   lin = Lasso(alpha=0.0001,precompute=True,max_iter=1000,
               positive=True, random_state=9999, selection='random')
-  lin.fit(z_reference, z_signal)
-  z_reference_fitted = lin.predict(z_reference).reshape(len(z_reference),1) 
+  n = len(reference)
+  lin.fit(reference.reshape(n,1), signal.reshape(n,1))
+  reference = lin.predict(reference.reshape(n,1)).reshape(n,)
 
  # z dFF    
-  zdFF = (z_signal - z_reference_fitted)
+  zdFF = (signal - reference)
  
-  return zdFFf
+  return zdFF
